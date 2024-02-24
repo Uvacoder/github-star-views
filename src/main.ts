fc2222891ebb19eb549cc12e3c8b553c4d555b91
ssssota <tommysouta@gmail.com>
2024-02-24 12:27:12 +0900

    fix: use mizu.css instead of water.css

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
+import "mizu.css/light.css";
 import { mount } from "svelte";
-import "water.css/out/light.css";
 import App from "./App.svelte";
 
 const app = mount(App, { target: document.getElementById("app")! });
